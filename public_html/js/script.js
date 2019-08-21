3849f8b8d7817aeda6474dbbb97a502091b29ce7
Shaun Sproston <shaunsproston@yahoo.co.uk>
2019-08-21 20:36:49 +0100

    Initial Setup

---
+++ public_html/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(()=>{
+    
+    $('.tab-arrow').on('click', (event)=> {
+        $(event.currentTarget).parent().siblings().slideToggle();
+        $(event.currentTarget).toggleClass('rotate');
+    })
+
+    $('#menu-icon').on('click', (event)=> {
+        $('.hamburger-menu-background').slideToggle();
+        $('.hamburger-menu li').slideToggle();
+    })
+
+    $('#book-btn').on('click', (event)=> {
+        $('.form-container').slideToggle();
+    })
+
+});
+
